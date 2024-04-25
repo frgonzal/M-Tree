4bf5c09860919d68384ade2bff9b6dbb1210f922
Franco Gonzalez <fr.gonzalezleiva@gmail.com>
2024-04-24 22:15:49 -0400

    feat(random_generator): implementado generador de puntos aleatorios

---
+++ headers/random_generator.h
@@ -0,0 +1,13 @@
+#ifndef RANDOM_GENERATOR_H
+#define RANDOM_GENERATOR_H
+
+/** Genera un archivo con random Points
+*   Los puntos tienen coordenadas con valores entre 0 y 1
+*   
+*   @param filename El nombre del archivo
+*   @param size     El numero de puntos a generar
+*/
+void random_sample_generator(char *filename, int size);
+
+
+#endif
