3ff0cea6a477203a9d00fa9f9e7bd4f95f06c8f9
Franco Gonzalez <fr.gonzalezleiva@gmail.com>
2024-04-25 22:17:30 -0400

    changes(search): Cambios en la implementacion. Ahora esta mejor. Aun falta testear

--- src/mtree.c
+++ src/mtree.c
@@ -4,76 +4,86 @@
 #include "../headers/point.h"
 
 
-/*
-typedef struct linked_list{
-    Point p;
-    linked_list *next;
-} LinkedList;
-
-
-Point* to_array(LinkedList *LinkedList, int *size){
-    *size = 0;
-
-    for(LinkedList *node=linked_list; node!=NULL; node=node->next)
-        *size++;
-
-    Point* points = (Points*) malloc(size*sizeof(Point));
-
-    LinkedList *node=linked_list;
-    while(LinkedList *node=linked_list; node!=NULL; node=node->next){
-        points[i] = node->p;
-    }
-
-    return points;
-}
+/** Variable global para la posicion en la que se debe 
+*   guardar el siguiente punto en la query
 */
+static int pos;
 
 
-void recur_search(MTree *mtree, Point q, double r, Point* points, int* pos);
-
-/** Calcula el largo del array */
-int search_size(MTree *mtree, Point q, double r, int *size){
+/** Calcula el numero de puntos del resultado.
+*   Util para pedir memoria para el resultado.
+*
+*   @param mtree  Puntero al mtree
+*   @param q      Punto central del radio de busqueda
+*   @param r      Radio de busqueda
+*   @param points Array de puntos donde se guardaran los resultados
+*   
+*   @return el numero de lecturas que se hacen en disco
+*/
+static int query_size(MTree *mtree, Point q, double r){
     double dist_pq = dist(mtree->p, q);
+    int n = 0;
 
-    if(mtree->a == NULL && dist_pq <= r)
-        (*size)++;
-
-    if(mtree->a != NULL && dist_pq <= mtree->cr + r){
-        for(int i=0; i<mtree->n; i++){
-            recur_search(mtree, q, r, NULL, NULL);
-        }
-    }   
-    return 0;
-}
-
-
-/** Busca todos los puntos que estan en la query y los agrega a un array*/
-void recur_search(MTree *mtree, Point q, double r, Point* points, int* pos){
-    double dist_pq = dist(mtree->p, q);
-
-    if(mtree->a == NULL && dist_pq <= r)
-        points[*pos] = mtree->p;
-
-    if(mtree->a != NULL && dist_pq <= mtree->cr + r){
-        for(int i=0; i<mtree->n; i++){
-            recur_search(mtree, q, r, points, pos);
-        }
+    if(mtree->a == NULL && dist_pq <= r){
+        return 1;
     }
+
+    if(mtree->a != NULL && dist_pq <= mtree->cr + r){
+        for(int i=0; i<mtree->n; i++)
+            n += query_size((mtree->a)+i, q, r);
+    }   
+    return n;
 }
 
 
-Point* search(MTree *mtree, Point q, double r, int *size_arr, int *n_querys){
-    search_size(mtree, q, r, size_arr);
+/** Busca todos los puntos que estan en la query y los agrega a un array.
+*   
+*   @param mtree  Puntero al mtree
+*   @param q      Punto central del radio de busqueda
+*   @param r      Radio de busqueda
+*   @param points Array de puntos donde se guardaran los resultados
+*   
+*   @return el numero de lecturas que se hacen en disco
+*/
+static int query(MTree *mtree, Point q, double r, Point* points){
+    int n = 1;
+
+    double dist_pq = dist(mtree->p, q);
+
+    if(mtree->a == NULL && dist_pq <= r){
+        points[pos] = mtree->p;
+        pos++;
+        return n;
+    }
+
+    if(mtree->a != NULL && dist_pq <= mtree->cr + r){
+        for(int i=0; i<mtree->n; i++)
+            n += query((mtree->a)+i, q, r, points);
+    }
+
+    return n;
+}
+
+
+Point* search(MTree *mtree, Point q, double r, int *size_arr, int *IOs){
+    // calculo del largo del array
+    *size_arr = query_size(mtree, q, r);
     Point *points = (Point*) malloc((*size_arr)*sizeof(Point));
 
-    int pos = 0;
-    recur_search(mtree, q, r, points, &pos);
+    // guardar los valores
+    pos = 0;
+    *IOs = query(mtree, q, r, points);
 
     return points;
 }
 
 
 void destroy(MTree *mtree){
+    if(mtree->a != NULL){
+        for(int i=0; i<mtree->n; i++)
+            destroy( (mtree->a)+i);
 
-    return;
+        free(mtree->a);
+        mtree->a = NULL;
+    }
 }
