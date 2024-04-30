b2a51c3fddad4d65e9f4a844f5e855ea50c83e18
Franco Gonzalez <fr.gonzalezleiva@gmail.com>
2024-04-30 17:55:11 -0400

    feat(Point): swap fun for points

--- src/point.c
+++ src/point.c
@@ -12,3 +12,9 @@ double dist(Point p1, Point p2){
 int point_equal(Point p1, Point p2){
     return (p1.x == p2.x) && (p1.y == p2.y);
 }
+
+void swap_points(Point *p1, Point *p2) {
+    Point temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+}
