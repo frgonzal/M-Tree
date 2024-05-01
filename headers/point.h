f80ebf03c427f3f4917d1605628c3a3dcd649552
Franco Gonzalez <fr.gonzalezleiva@gmail.com>
2024-05-01 19:18:54 -0400

    feat(point): added dist2

--- headers/point.h
+++ headers/point.h
@@ -11,22 +11,34 @@ typedef struct {
     double y;
 } Point;
 
-/** Calcula distancia entre 2 puntos 
+/** Distance between two points
 *
-*   @param p1 punto 1
-*   @param p2 punto 2
+*   @param p1
+*   @param p2
 *   
-*   @return distancia entre los puntos
+*   @return Distance
 */
 double dist(Point p1, Point p2);
 
+/** Square distance between two points 
+*
+*   @param p1
+*   @param p2
+*   
+*   @return Distance
+*/
+double dist2(Point p1, Point p2);
 
-/** Comprueba si dos puntos son iguales */
+
+/** Returns true if the distance between two points is 0 */
 int point_equal(Point p1, Point p2);
 
 /** Swaps to points on an array */
 void swap_points(Point *p1, Point *p2);
 
+/** Compares to points
+*   useful for vector
+*/
 int cmp_points(void *pp1, void *pp2);
 
 #endif
--- src/mtree/point.c
+++ src/mtree/point.c
@@ -9,6 +9,13 @@ double dist(Point p1, Point p2){
     return sqrt(diff_x*diff_x + diff_y*diff_y);
 }
 
+double dist2(Point p1, Point p2){
+    double diff_x = p1.x - p2.x;
+    double diff_y = p1.y - p2.y;
+
+    return diff_x*diff_x + diff_y*diff_y;
+}
+
 int point_equal(Point p1, Point p2){
     return (p1.x == p2.x) && (p1.y == p2.y);
 }
