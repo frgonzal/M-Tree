781b443bfaba5d5427c2644d8f5fed4847bafa1a
Franco Gonzalez <fr.gonzalezleiva@gmail.com>
2024-05-01 20:09:33 -0400

    change(mtree): constructors use (Point const *)

--- src/mtree/mtree_create_cp.c
+++ src/mtree/mtree_create_cp.c
@@ -2,7 +2,7 @@
 #include "../../headers/mtree.h"
 #include "../../headers/point.h"
 
-MTree* mtree_create_cp(Point *points, int n){
+MTree* mtree_create_cp(Point const *points, int n){
 
     return NULL;
 }
--- src/mtree/mtree_create_ss.c
+++ src/mtree/mtree_create_ss.c
@@ -2,7 +2,7 @@
 #include "../../headers/mtree.h"
 #include "../../headers/point.h"
 
-MTree* mtree_create_ss(Point *points, int n){
+MTree* mtree_create_ss(Point const *points, int n){
 
     return NULL;
 }
