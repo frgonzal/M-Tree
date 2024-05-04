6665158c6be48da4a34b2caa7c9a3b5af2f58ef5
Franco Gonzalez <fr.gonzalezleiva@gmail.com>
2024-05-04 15:25:45 -0400

    feat(ss): added some functions

--- src/mtree/mtree_create_ss.cpp
+++ src/mtree/mtree_create_ss.cpp
@@ -1,35 +1,96 @@
+#include <cstddef>
 #include <stdio.h>
 #include <vector>
+#include <tuple>
 #include "../../headers/mtree.hpp"
 #include "../../headers/point.hpp"
 
-#define INFINITY 1e9;
+#define MAX 1e9;
 
-static void outout_hoja();
+typedef std::tuple<Point, double, MTree*> tupleGRA;
+
+
+static tupleGRA output_hoja(const std::vector<Point> &cluster);
+
+static tupleGRA output_interno(std::vector<tupleGRA> &tuplas);
+
+static std::vector<std::vector<Point>> cluster(const std::vector<Point> &cluster);
+
+static Point find_medoid(const std::vector<Point> &points);
+
+static double cluster_dist(const std::vector<Point> &cluster1, const std::vector<Point> &cluster2);
+
+
+
 
 MTree* mtree_create_ss(const std::vector<Point> &points){
+    /* Si |Cin| ≤ B: Se define (g, r, a) = OutputHoja(Cin) y se retorna a */
     if (points.size() <= B) {
-        //std::tuple<, double, MTree*>
-        //return OutputHoja(&cluster);
-        return nullptr;
+        std::tuple<Point, double, MTree*> t = output_hoja(points);
+        return std::get<2>(t);
     }
 
-  Vector *clusters = Cluster(points, b, B);
-  Vector *nodes = vec_init_(vec_len(clusters), sizeof(MTree *));
-  // Llenar 'nodes' con nodos de hoja para cada cluster, no se tampoco
+    /* Sea C_out = Cluster(C_in). Sea C = {}. */
+    std::vector<std::vector<Point>> C_out = cluster(points);
+    std::vector<tupleGRA> C(C_out.size());
 
-  while (vec_len(nodes) > B) {
-    Vector *newClusters = Cluster(/* extraer puntos de nodos */, b, B);
-    Vector *newNodes = vec_init_(vec_len(newClusters), sizeof(MTree *));
-    // Llenar los 'newNodes' con nodos internos, no se como chucha aun
-    vec_destroy(nodes);
-    nodes = newNodes;
-  }
+    /* Por cada c ∈ Cout: Se añade OutputHoja(c) a C */
+    for(int i=0; i<C_out.size(); i++)
+        C[i] = output_hoja(C_out[i]);
 
-  MTree *root = OutputInterno(nodes);
-  vec_destroy(nodes);
-  return root;
+    /* Mientras |C| > B: */
+    while(C.size() > B){
+        /* Sea Cin = {g|(g, r, a) ∈ C}. Sea Cout = Cluster(Cin). Sea Cmra = {}.*/
+        std::vector<Point> C_in(C.size());
+        for(int i=0; i<C.size(); i++)
+            C_in[i] = std::get<0>(C[i]); // C_in[i] = g
+
+        C_out = cluster(C_in);
+
+        std::vector<tupleGRA> C_mra;
+
+        /* Por cada c ∈ Cout: Sea s = {(g, r, a)|(g, r, a) ∈ C ∧ g ∈ c}, se añade s a Cmra */
+
+
+        /* Sea C = {}. */
+
+        /* Por cada s ∈ Cmra: Añadir OutputInterno(s) a C */
+    }
+
+    /* Sea (g, r, a) = OutputInterno(C) */
+    /* Se retorna a */
+    return std::get<2>(output_interno(C));
 }
+
+
+/** El medoide de un cluster es un punto g en el cluster tal que no existe otro punto p en el cluster
+*   que, al ser nominado como medoide, genere un radio menor al entregado por g 
+*/
+static Point find_medoid(const std::vector<Point>& points) {
+    double min_sum = MAX;
+    Point medoid;
+
+    for (int i=0; i<points.size(); i++) {
+        double sum = 0;
+        for (int j=0; j<points.size(); j++)
+            sum += dist(points[i], points[j]);
+
+        if (sum < min_sum) {
+            medoid = points[i];
+            min_sum = sum;
+        }
+    }
+    return medoid;
+}
+
+
+/** La distancia entre dos clusters es la distancia entre sus medoides primarios. */
+static double cluster_dist(std::vector<Point> &cluster1, std::vector<Point> &cluster2) {
+    Point medoid1 = find_medoid(cluster1);
+    Point medoid2 = find_medoid(cluster2);
+    return dist(medoid1, medoid2);
+}
+
 
 // Función para asignar cada punto a su centro de cluster (sample) más cercano
 void assign_points_to_closest_sample(Point *points, int total_points, Point *samples, int total_samples, int *closest_sample_indices) {
@@ -52,122 +113,90 @@ void assign_points_to_closest_sample(Point *points, int total_points, Point *sam
 }
 
 
-
 Vector *Cluster(Vector *points, int b, int B) {
-  Vector *C = vec_init_(vec_len(points), sizeof(Vector *));
-  Vector *C_Res = vec_init_(0, sizeof(Vector *)); 
+    Vector *C = vec_init_(vec_len(points), sizeof(Vector *));
+    Vector *C_Res = vec_init_(0, sizeof(Vector *)); 
 
-  // 1: Crear clusters con un solo punto 
-  for (int i = 0; i < vec_len(points); i++) {
-    Point *p = (Point *)vec_get(points, i);
-    Vector *singlePointCluster = vec_init_(1, sizeof(Point));
-    vec_push(singlePointCluster, p);
-    vec_push(C, &singlePointCluster); 
-  }
+    // 1: Crear clusters con un solo punto 
+    for (int i = 0; i < vec_len(points); i++) {
+        Point *p = (Point *)vec_get(points, i);
+        Vector *singlePointCluster = vec_init_(1, sizeof(Point));
+        vec_push(singlePointCluster, p);
+        vec_push(C, &singlePointCluster); 
+    }
 
-  // 2: Combinar clusters
-  while (vec_len(C) > 1) {
-    int minDistance = INT_MAX;
-    int bestPair[2] = {-1, -1};
+    // 2: Combinar clusters
+    while (vec_len(C) > 1) {
+        int minDistance = INT_MAX;
+        int bestPair[2] = {-1, -1};
 
-    // par más cercano de clusters
-    for (int i = 0; i < vec_len(C) - 1; i++) {
-      for (int j = i + 1; j < vec_len(C); j++) {
-        Vector *cluster1 = *(Vector **)vec_get(C, i);
-        Vector *cluster2 = *(Vector **)vec_get(C, j);
-        int dist = cluster_dist(cluster1, cluster2); 
+        // par más cercano de clusters
+        for (int i = 0; i < vec_len(C) - 1; i++) {
+            for (int j = i + 1; j < vec_len(C); j++) {
+                Vector *cluster1 = *(Vector **)vec_get(C, i);
+                Vector *cluster2 = *(Vector **)vec_get(C, j);
+                int dist = cluster_dist(cluster1, cluster2); 
 
-        if (dist < minDistance && (vec_len(cluster1) + vec_len(cluster2) <= B)) {
-          minDistance = dist;
-          bestPair[0] = i;
-          bestPair[1] = j;
+                if (dist < minDistance && (vec_len(cluster1) + vec_len(cluster2) <= B)) {
+                    minDistance = dist;
+                    bestPair[0] = i;
+                    bestPair[1] = j;
+                }
+            }
         }
-      }
+
+        // Juntar los clusters más cercanos
+        if (bestPair[0] != -1 && bestPair[1] != -1) {
+            Vector *newCluster = combine_clusters(*(Vector **)vec_get(C, bestPair[0]), *(Vector **)vec_get(C, bestPair[1]));
+            vec_remove(C, bestPair[1]); 
+            vec_remove(C, bestPair[0]);
+            vec_push(C, &newCluster);
+        } else {
+            // Si no se pueden combinar más clusters, mover todos los clusters restantes a C_Res ??????? Gracias GPAPITO, MEDIO ERROR ME ENCONTRASTE
+            while (vec_len(C) > 0) {
+                Vector *remainingCluster = *(Vector **)vec_pop(C, 0);
+                vec_push(C_Res, &remainingCluster);
+            }
+        }
     }
 
-    // Juntar los clusters más cercanos
-    if (bestPair[0] != -1 && bestPair[1] != -1) {
-      Vector *newCluster = combine_clusters(*(Vector **)vec_get(C, bestPair[0]), *(Vector **)vec_get(C, bestPair[1]));
-      vec_remove(C, bestPair[1]); 
-      vec_remove(C, bestPair[0]);
-      vec_push(C, &newCluster);
-    } else {
-      // Si no se pueden combinar más clusters, mover todos los clusters restantes a C_Res ??????? Gracias GPAPITO, MEDIO ERROR ME ENCONTRASTE
-      while (vec_len(C) > 0) {
-        Vector *remainingCluster = *(Vector **)vec_pop(C, 0);
-        vec_push(C_Res, &remainingCluster);
-      }
-    }
-  }
-
-  vec_destroy(C); 
-  return C_Res;
-}
-
-MTree *OutputHoja(Cluster *cluster) {
-  MTree *node = malloc(sizeof(MTree));
-  *node = (MTree){cluster->medoid, 0, NULL, 0, 0};
-  return node;
+    vec_destroy(C); 
+    return C_Res;
 }
 
 MTree *OutputInterno(Vector *children) {
-  if (vec_len(children) == 0)
-    return NULL;
+    if (vec_len(children) == 0)
+        return NULL;
 
-  Point medoid =
-      /* función para calcular el medoide de los medoides de los hijos */;
-  double max_radius = 0;
-  MTree *node = malloc(sizeof(MTree));
-  node->a = malloc(vec_len(children) * sizeof(MTree));
+    Point medoid =
+        /* función para calcular el medoide de los medoides de los hijos */;
+    double max_radius = 0;
+    MTree *node = malloc(sizeof(MTree));
+    node->a = malloc(vec_len(children) * sizeof(MTree));
 
-  for (int i = 0; i < vec_len(children); ++i) {
-    MTree *child = (MTree *)vec_get(children, i);
-    double distance = dist(medoid, child->p) + child->cr;
-    max_radius = fmax(max_radius, distance);
-  }
+    for (int i = 0; i < vec_len(children); ++i) {
+        MTree *child = (MTree *)vec_get(children, i);
+        double distance = dist(medoid, child->p) + child->cr;
+        max_radius = fmax(max_radius, distance);
+    }
 
-  *node = (MTree){medoid, max_radius, node->a, vec_len(children),
+    *node = (MTree){medoid, max_radius, node->a, vec_len(children),
                   1 + ((MTree *)vec_get(children, 0))->h};
-  return node;
+    return node;
 }
 
-int cluster_dist(Vector *cluster1, Vector *cluster2) {
-  Point medoid1 = medoid_of_cluster(cluster1);
-  Point medoid2 = medoid_of_cluster(cluster2);
-  return dist(medoid1, medoid2);
-}
+
 
 Vector *combine_clusters(Vector *cluster1, Vector *cluster2) {
-  Vector *newCluster =
-      vec_init_(vec_len(cluster1) + vec_len(cluster2), sizeof(Point));
-  for (int i = 0; i < vec_len(cluster1); i++) {
-    Point *p = (Point *)vec_get(cluster1, i);
-    vec_push(newCluster, p);
-  }
-  for (int j = 0; j < vec_len(cluster2); j++) {
-    Point *p = (Point *)vec_get(cluster2, j);
-    vec_push(newCluster, p);
-  }
-  return newCluster;
-}
-
-Point medoid_of_cluster(Vector *cluster) {
-  Point bestMedoid;
-  double minDistanceSum = DBL_MAX;
-  // Recorrer cada punto en el cluster y calcular la suma de distancias a todos los otros puntos
-  for (int i = 0; i < vec_len(cluster); i++) {
-    Point *currentPoint = (Point *)vec_get(cluster, i);
-    double distanceSum = 0;
-    for (int j = 0; j < vec_len(cluster); j++) {
-      if (i != j) {
-        Point *otherPoint = (Point *)vec_get(cluster, j);
-        distanceSum += dist(*currentPoint, *otherPoint);
-      }
+    Vector *newCluster =
+        vec_init_(vec_len(cluster1) + vec_len(cluster2), sizeof(Point));
+    for (int i = 0; i < vec_len(cluster1); i++) {
+        Point *p = (Point *)vec_get(cluster1, i);
+        vec_push(newCluster, p);
     }
-    if (distanceSum < minDistanceSum) {
-      minDistanceSum = distanceSum;
-      bestMedoid = *currentPoint;
+    for (int j = 0; j < vec_len(cluster2); j++) {
+        Point *p = (Point *)vec_get(cluster2, j);
+        vec_push(newCluster, p);
     }
-  }
-  return bestMedoid;
+    return newCluster;
 }
